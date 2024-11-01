5d73dadfde5e728faa0e250e4186b26663912d46
MARUNGs <mi1764@naver.com>
2024-11-01 13:57:40 +0900

    gesture and drag 2

--- src/components/GesturesAndDrag.tsx
+++ src/components/GesturesAndDrag.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 // styled -----
 const Wrapper = styled.div`
@@ -18,6 +19,17 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
 `;
 
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 // motion -----
 const boxVars = {
   hover: {
@@ -28,6 +40,7 @@ const boxVars = {
     scale: 1,
     borderRadius: "100px",
   },
+  /*
   drag: {
     // color에 관련된 값을 문자열이 아닌, 숫자값으로 부여하면 motion이 애니메이션 처리해준다.
     backgroundColor: "rgb(26, 188, 156)",
@@ -35,9 +48,12 @@ const boxVars = {
       duration: 10,
     },
   },
+  */
 };
 
 function GesturesAndDrag() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <>
       <Wrapper>
@@ -47,13 +63,23 @@ function GesturesAndDrag() {
           (문자열이 매칭되면 된다)
           ex. whileHover={condition ? "hover" : "click"}
         */}
-        <Box
-          drag
-          variants={boxVars}
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-        />
+        <BiggerBox ref={biggerBoxRef}>
+          <Box
+            // drag="x": x축만 드래그 가능 // drag="y": y축만 드래그 가능
+            drag
+            // dragConstraints: 드래그가 허용되는 영역
+            // ref 개념을 이용하여 처리가능(ref: 특정 요소와 바인딩할 수 있는 prop)
+            dragConstraints={biggerBoxRef}
+            // 드래그 이후 다시 정중앙으로 돌려보내기
+            // dragSnapToOrigin={}
+            // dragElastic: 당기는 힘(default: 0.5) 0 ~ 1 설정가능
+            dragElastic={0.5}
+            variants={boxVars}
+            whileHover="hover"
+            whileTap="click"
+            whileDrag="drag"
+          />
+        </BiggerBox>
       </Wrapper>
     </>
   );
