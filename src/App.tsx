b91f70a8438c49310cfbfe9b0c9b5a6314268fe7
MARUNGs <mi1764@naver.com>
2024-11-03 23:37:47 +0900

    주석제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { GlobalStyle } from "./styles/\bAppStyled";
-// import Variants from "./components/Variants";
-// import GesturesAndDrag from "./components/GesturesAndDrag";
-// import MotionValue from "./components/MotionValue";
-// import Scroll from "./components/Scroll";
-// import Svg from "./components/Svg";
-// import AnimatePresenceBox from "./components/AnimatePresence";
-// import LayerBox from "./components/Layer";
+import Variants from "./components/Variants";
+import GesturesAndDrag from "./components/GesturesAndDrag";
+import MotionValue from "./components/MotionValue";
+import Scroll from "./components/Scroll";
+import Svg from "./components/Svg";
+import AnimatePresenceBox from "./components/AnimatePresence";
+import LayerBox from "./components/Layer";
 import GridBox from "./components/GridBox";
 
 // main
@@ -14,13 +14,13 @@ function App() {
     <>
       <GlobalStyle />
       {/* <Variants /> */}
-      {/* <GesturesAndDrag /> */}
+      <GesturesAndDrag />
       {/* <MotionValue /> */}
       {/* <Scroll /> */}
       {/* <Svg /> */}
       {/* <AnimatePresenceBox /> */}
       {/* <LayerBox /> */}
-      <GridBox />
+      {/* <GridBox /> */}
     </>
   );
 }
